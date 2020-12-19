a6448d94b59f7abb0076715170ad66ec879a491f
LiqiaoHuang <76257343+LiqiaoHuang@users.noreply.github.com>
2020-12-19 23:40:42 +0800

    Update china_map_2.js

--- examples/district/basic/demo/china_map_2.js
+++ examples/district/basic/demo/china_map_2.js
@@ -4,102 +4,82 @@ import { Mapbox } from '@antv/l7-maps';
 const colors = [ '#B8E1FF', '#7DAAFF', '#3D76DD', '#0047A5', '#001D70' ];
 const ProvinceData = [
   {
-    name: '云南省',
     code: 530000,
     value: 17881.12
   },
   {
-    name: '黑龙江省',
     code: 230000,
     value: 16361.62
   },
   {
-    name: '贵州省',
     code: 520000,
     value: 14806.45
   },
   {
-    name: '北京市',
     code: 110000,
     value: 30319.98
   },
   {
-    name: '河北省',
     code: 130000,
     value: 36010.27
   },
   {
-    name: '山西省',
     code: 140000,
     value: 16818.11
   },
   {
-    name: '吉林省',
     code: 220000,
     value: 15074
   },
   {
-    name: '宁夏回族自治区',
     code: 640000,
     value: 3705.18
   },
   {
-    name: '辽宁省',
     code: 210000,
     value: 25315.35
   },
   {
-    name: '海南省',
     code: 460000,
     value: 4832.05
   },
   {
-    name: '内蒙古自治区',
     code: 150000,
     value: 17289.22
   },
   {
-    name: '天津市',
     code: 120000,
     value: 18809.64
   },
   {
-    name: '新疆维吾尔自治区',
     code: 650000,
     value: 12199.08
   },
   {
-    name: '上海市',
     code: 310000,
     value: 32679.87
   },
   {
-    name: '陕西省',
     code: 610000,
     value: 24438.32
   },
   {
-    name: '甘肃省',
     code: 620000,
     value: 8246.07
   },
   {
-    name: '安徽省',
     code: 340000,
     value: 30006.82
   },
   {
-    name: '香港特别行政区',
     code: 810000,
     value: 0
   },
   {
-    name: '广东省',
     code: 440000,
     value: 97277.77
   },
   {
-    name: '河南省',
     code: 410000,
     value: 48055.86
   },
